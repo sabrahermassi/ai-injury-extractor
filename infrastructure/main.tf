92e8b8e45c063b8e747f010b442760ed1c6d2c15
sabra <sabra.herm@gmail.com>
2026-08-05 14:57:16 +0200

    Add DynamoDB table infrastructure with Terraform

---
+++ infrastructure/main.tf
@@ -0,0 +1,22 @@
+resource "aws_dynamodb_table" "injury_entries" {
+  name         = "InjuryEntries"
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key  = "userId"
+  range_key = "timestamp"
+
+  attribute {
+    name = "userId"
+    type = "S"
+  }
+
+  attribute {
+    name = "timestamp"
+    type = "S"
+  }
+
+  tags = {
+    Project = "ai-injury-extractor"
+    Environment = "dev"
+  }
+}
